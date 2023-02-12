7087a24c204c71d65fa46a2473bbd710158c1100
Azmi Ibrahim <abuzahrah75@gmail.com>
2023-02-12 21:56:07 +0800

    add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.7
+
+WORKDIR /app
+
+RUN pip install --upgrade pip
+COPY requirements.txt /app/requirements.txt
+RUN pip install -r requirements.txt
+
+EXPOSE 8083
+
+CMD python manage.py migrate && python manage.py runserver 0.0.0.0:8083
--- abuzahrah2023/settings.py
+++ abuzahrah2023/settings.py
@@ -90,9 +90,17 @@ WSGI_APPLICATION = 'abuzahrah2023.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'abuzahrah2023',
+        'USER': 'abuzahrah',
+        'PASSWORD': 'zahrah@75',
+        'HOST': 'zahrah',
+        'PORT': '8107',
     }
 }
 
