b04d6ae416f200aa4762ffbd211b04946d954bda
Azmi Ibrahim <abuzahrah75@gmail.com>
2023-02-24 11:33:09 +0800

    after ignore settings.py

--- abuzahrah2023/settings.py
+++ abuzahrah2023/settings.py
@@ -95,32 +95,32 @@ WSGI_APPLICATION = 'abuzahrah2023.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-       
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('DB_NAME', 'mydatabase'),
-            'USER': os.environ.get('DB_USER', 'mydatabaseuser'),
-            'PASSWORD': os.environ.get('DB_PASSWORD', 'mypassword'),
-            'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-            'PORT': os.environ.get('DB_PORT', '5432'),
-        },
-}
-
 # DATABASES = {
-#        # 'default': {
-#        #     'ENGINE': 'django.db.backends.sqlite3',
-#        #     'NAME': BASE_DIR / 'db.sqlite3',
-#        # }
-#        'default': {
-#            'ENGINE': 'django.db.backends.postgresql',
-#            'NAME': os.environ.get('DB_NAME', 'abuzahrah02'),
-#            'USER': os.environ.get('DB_USER', 'abuzahrah'),
-#            'PASSWORD': os.environ.get('DB_PASSWORD', 'zahrah@75'),
-#            'HOST': os.environ.get('DB_HOST', '165.22.63.162'),
-#            'PORT': os.environ.get('DB_PORT', '8107'),
-#        },
+       
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': os.environ.get('DB_NAME', 'mydatabase'),
+#             'USER': os.environ.get('DB_USER', 'mydatabaseuser'),
+#             'PASSWORD': os.environ.get('DB_PASSWORD', 'mypassword'),
+#             'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+#             'PORT': os.environ.get('DB_PORT', '5432'),
+#         },
 # }
+
+DATABASES = {
+       # 'default': {
+       #     'ENGINE': 'django.db.backends.sqlite3',
+       #     'NAME': BASE_DIR / 'db.sqlite3',
+       # }
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': os.environ.get('DB_NAME', 'abuzahrah02'),
+           'USER': os.environ.get('DB_USER', 'abuzahrah'),
+           'PASSWORD': os.environ.get('DB_PASSWORD', 'zahrah@75'),
+           'HOST': os.environ.get('DB_HOST', '165.22.63.162'),
+           'PORT': os.environ.get('DB_PORT', '8107'),
+       },
+}
 
 
 # Password validation
