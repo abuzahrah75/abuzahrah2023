6e16fadd14d5c4728f90e58c7292001d696f3995
Azmi Ibrahim <69234876+abuzahrah75@users.noreply.github.com>
2023-03-08 09:28:07 +0800

    Update settings_old.py

--- abuzahrah2023/settings_old.py
+++ abuzahrah2023/settings_old.py
@@ -108,18 +108,11 @@ WSGI_APPLICATION = 'abuzahrah2023.wsgi.application'
 # }
 
 DATABASES = {
-       # 'default': {
-       #     'ENGINE': 'django.db.backends.sqlite3',
-       #     'NAME': BASE_DIR / 'db.sqlite3',
-       # }
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': os.environ.get('DB_NAME', 'abuzahrah02'),
-           'USER': os.environ.get('DB_USER', 'abuzahrah'),
-           'PASSWORD': os.environ.get('DB_PASSWORD', 'zahrah@75'),
-           'HOST': os.environ.get('DB_HOST', '165.22.63.162'),
-           'PORT': os.environ.get('DB_PORT', '8107'),
-       },
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+      
 }
 
 
